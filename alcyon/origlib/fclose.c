3a78d7bb629e3c5aa1ce0c6b05f36c288ab5ed6d
Thorsten Otto <thotto@users.sourceforge.net>
2016-08-16 01:31:00 +0200

    lib: fclose

---
+++ alcyon/origlib/fclose.c
@@ -0,0 +1,36 @@
+/*********************************************************************
+*
+*			f c l o s e   F u n c t i o n
+*			-----------------------------
+*	Copyright 1982 by Digital Research Inc.  All rights reserved.
+*
+*	"fclose" flushes a stream (buffered file) and releases the
+*	channel and any allocated buffers.
+*
+*	Calling sequence:
+*		ret = fclose(stream)
+*	Where:
+*		stream -> file to be closed (FILE *)
+*		ret = SUCCESS or FAILURE (if IO error)
+*
+*****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int fclose(P(FILE *) sp)
+PP(register FILE *sp;)
+{
+	if (sp->_flag & (_IOREAD | _IOWRT))	/* is it closeable?     */
+	{									/* yup...           */
+		fflush(sp);						/* do the flush         */
+		if (sp->_flag & _IOABUF)		/* was buf alloc'd?     */
+			free(sp->_base);			/* free it ifso         */
+		sp->_base = sp->_ptr = 0;		/* reset these          */
+		sp->_cnt = 0;
+	}
+	/* reset all flags */
+	sp->_flag &= ~(_IOREAD | _IOWRT | _IOABUF | _IONBUF | _IOERR | _IOEOF | _IOLBUF);
+	/* and return */
+	return close(sp->_fd);
+}
