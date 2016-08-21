af73c4f15df02e8a19eb83292da66b0146508edf
Thorsten Otto <thotto@users.sourceforge.net>
2016-08-21 12:48:50 +0200

    lib: chown/chmod stubs

--- alcyon/orig/libsrc/chmod.c
+++ alcyon/orig/libsrc/chmod.c
@@ -1,1 +1,18 @@
-extern int errno;
+#include <osif.h>
+#include "lib.h"
+#include <osiferr.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/***************************************/
+/* chmod - change mode: NOP under CP/M */
+/***************************************/
+int chmod(P(const char *) name, P(mode_t) mode)
+PP(const char *name;)
+PP(mode_t mode;)
+{
+	/* BUG: mode is a total different thing here */
+	return access(name, mode);
+}
--- alcyon/orig/libsrc/chown.c
+++ alcyon/orig/libsrc/chown.c
@@ -1,1 +1,20 @@
-extern int errno;
+#include <osif.h>
+#include "lib.h"
+#include <osiferr.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**************************************************/
+/* chown - change owner: like access() under CP/M */
+/**************************************************/
+int chown(P(const char *) name, P(uid_t) owner, P(gid_t) group)
+PP(const char *name;)
+PP(uid_t owner;)
+PP(gid_t group;)
+{
+	UNUSED(owner);
+	UNUSED(group);
+	return access(name, F_OK);
+}
